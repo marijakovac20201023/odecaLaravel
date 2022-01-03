1a3bc17422cd01d3224668a9403bf19cce3d4061
marijakovac20201023 <kovacmarija307@gmail.com>
2022-01-03 21:39:43 +0100

    kreiran osnovni model, migracije, seeder-i i factory

---
+++ app/Models/Odeca.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Odeca extends Model
+{
+    use HasFactory;
+}
